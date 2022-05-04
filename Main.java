ee8e0537183f72263bf4c925d6e4726ab86a62db
366-8f1-u1 <emre.cakar@hs-hannover.de>
2022-05-04 08:49:27 +0200

    Ausgabe in Deutsch: Hallo Welt

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 
 public  class Main
 {
+    /*
+     * Mein neuer Kommentar
+     */
     public static void main(String args[])
     {
-        System.out.println ("Hello world!");
+        System.out.println ("Hallo Welt!");
     }
 }
